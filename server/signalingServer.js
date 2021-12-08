c85f3396311678437363417d3312c19db5be415f
신 동준 <sindongjun@sins-MacBook-Pro.local>
2021-12-08 16:57:49 +0900

    feat: 브라우저 닫을 때 통계 및 스터디룸 인원 수 감소 로직 추가.

--- server/signalingServer.js
+++ server/signalingServer.js
@@ -1,8 +1,61 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server, {
+const app = require('express')();
+const request = require('request');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
   path: '/signaling/socket.io',
 });
+
+const postUserStatistics = (userId, studyTime, today) => {
+  let options = {
+    uri: `http://118.67.133.19:8080/statistics`,
+    method: 'POST',
+    body: {
+      userId: userId,
+      studyTime: studyTime,
+      date: today,
+    },
+    json: true,
+  };
+  request.post(options, (error, response, body) => {
+    if (error) console.log(error);
+  });
+};
+
+const getStudyRoomOut = (studyroom_id) => {
+  request.get(
+    { uri: `http://118.67.133.19:8080/studyrooms/${studyroom_id}/out` },
+    (error, response, body) => {
+      if (error) console.log(error);
+    },
+  );
+};
+
+const getTime = (seconds) => {
+  var hour =
+    parseInt(seconds / 3600) < 10
+      ? '0' + parseInt(seconds / 3600)
+      : parseInt(seconds / 3600);
+  var min =
+    parseInt((seconds % 3600) / 60) < 10
+      ? '0' + parseInt((seconds % 3600) / 60)
+      : parseInt((seconds % 3600) / 60);
+  var sec = seconds % 60 < 10 ? '0' + (seconds % 60) : seconds % 60;
+  return hour + ':' + min + ':' + sec;
+};
+
+const leftPad = (value) => {
+  if (value >= 10) {
+    return value;
+  }
+  return `0${value}`;
+};
+
+const toStringByFormatting = (source, delimiter = '-') => {
+  const year = source.getFullYear();
+  const month = leftPad(source.getMonth() + 1);
+  const day = leftPad(source.getDate());
+  return [year, month, day].join(delimiter);
+};
 
 let users = new Map();
 let socketToRoom = new Map();
@@ -20,17 +73,31 @@ io.on('connection', (socket) => {
         socketId: socket.id,
         userName: data.userName,
         userId: data.userId,
+        studyStart: 0,
       });
     } else {
       users[data.room] = [
-        { socketId: socket.id, userName: data.userName, userId: data.userId },
+        {
+          socketId: socket.id,
+          userName: data.userName,
+          userId: data.userId,
+          studyStart: 0,
+        },
       ];
     }
     socketToRoom[socket.id] = data.room;
     socket.join(data.room);
+  });
+
+  socket.on('users_in_room', (data) => {
     const usersInThisRoom = users[data.room].filter(
       (user) => user.socketId !== socket.id,
     );
+
+    users[data.room].map((user) => {
+      if (user.socketId === socket.id) user['studyStart'] = Date.now() / 1000;
+    });
+
     io.sockets.to(socket.id).emit('all_users', usersInThisRoom);
   });
 
@@ -77,6 +144,16 @@ io.on('connection', (socket) => {
     const roomID = socketToRoom[socket.id];
     let room = users[roomID];
     if (room) {
+      const disconnectedUserInfo = room.filter((user) => user.socketId === socket.id);
+      if (disconnectedUserInfo[0].studyStart !== 0) {
+        const studyTime = getTime(
+          Math.floor(Date.now() / 1000 - disconnectedUserInfo[0].studyStart),
+        );
+        let today = toStringByFormatting(new Date());
+        postUserStatistics(disconnectedUserInfo[0].userId, studyTime, today);
+      }
+
+      getStudyRoomOut(roomID);
       room = room.filter((user) => user.socketId !== socket.id);
       users[roomID] = room;
       if (room.length === 0) {
